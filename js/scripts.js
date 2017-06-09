05b97ba480b27eacbd6bdcf58d39ef771dc19286
Max Scher <maxobaxo@gmail.com>
2017-06-09 10:34:08 -0700

    add functionality to count up to number provided

--- js/scripts.js
+++ js/scripts.js
@@ -1,20 +1,39 @@
 //business logic here:
 var pingPong = function(userInput) {
 
-  if (typeof userInput !== "number") {
+  if (isNaN(userInput) === true) {
     alert("Please enter a number!");
-  } else {
-    var userResult = userInput;
-    return userResult;
   }
+
+  var arrUpTo = [];
+  for (var i = userInput; i >= 1; i -= 1) {
+    $("ul").prepend("<li>" + i + "</li>");
+  }
+
+  // var userString = userInput.toString();
+  // var userArr = [];
+  //
+  // for (var i = 0; i < userString.length; i++) {
+  //   userArr.push(userString[i]);
+  //   $("ul").prepend("<li>" + userArr[i] + "</li>");
+  // }
+
+
+
+  // var userArr = userInput.split("");
+  // for (var i = 0; i < userArr.length; i++) {
+  //   userArr[i] = parseInt(userArr[i]);
+  // }
+  // return userArr;
 };
 
 //UI logic here:
 $(document).ready(function() {
   $(".main-form").submit(function(event) {
     event.preventDefault();
-    var userInput = $("input#entry").val();
+
+    var userInput = parseInt($("input#entry").val());
     var result = pingPong(userInput);
-    $(".result").text(result);
+    $(".result").show(result);
   });
 });
