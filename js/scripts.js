4ad763f14d08e271df57fd517a42e2e0504b142e
Max Scher <maxobaxo@gmail.com>
2017-06-09 15:19:26 -0700

    add logic for reverse order button

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,6 @@
 //business logic here:
 var pingPong = function(userInput) {
-
+  console.log(userInput);
   var isNumber = function(userInput) {
     var number = /^[a-zA-Z]+$/;
     if (userInput.value.match(number)) {
@@ -27,9 +27,36 @@ var pingPong = function(userInput) {
   }
 };
 
+var pongPing = function(userInput) {
+  var isNumber = function(userInput) {
+    var number = /^[a-zA-Z]+$/;
+    if (userInput.value.match(number)) {
+      return true;
+    } else {
+      return false;
+    }
+  };
+
+  if (isNumber === false || isNaN(userInput) === true) {
+    alert("Please enter a number!");
+  } else {
+    for (var i = parseInt(userInput); i >= 1; i -= 1) {
+      if (parseInt(i) % 3 === 0 && parseInt(i) % 5 === 0) {
+        $("ul").append("<li>pingpong</li>");
+      } else if (parseInt(i) % 5 === 0) {
+        $("ul").append("<li>pong</li>");
+      } else if (parseInt(i) % 3 === 0) {
+        $("ul").append("<li>ping</li>");
+      } else {
+        $("ul").append("<li>" + i + "</li>");
+      }
+    };
+  }
+};
+
 //UI logic here:
 $(document).ready(function() {
-  $(".main-form").submit(function(event) {
+  $("#normal").click(function(event) {
     event.preventDefault();
 
     $("ul").empty();
@@ -37,4 +64,27 @@ $(document).ready(function() {
     var result = pingPong(userInput);
     $(".result").show(result);
   });
+  $("#reverse").click(function(event) {
+    event.preventDefault();
+    $("ul").empty();
+    var userInput = $("input#entry").val();
+    var result = pongPing(userInput);
+    $(".result").show(result);
+  });
 });
+
+
+
+// if ($("button").attr("#normal")) {
+//   alert("normal button pressed")
+//   $("ul").empty();
+//   var userInput = $("input#entry").val();
+//   var result = pingPong(userInput);
+//   $(".result").show(result);
+// } else {
+//   alert("reverse button pressed")
+//   $("ul").empty();
+//   var userInput = $("input#entry").val();
+//   var result = pongPing(userInput);
+//   $(".result").show(result);
+// }
