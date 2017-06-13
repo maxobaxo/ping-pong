1e5a7554030e086e8ed38f516586a9f440a83d45
Max Scher <maxobaxo@gmail.com>
2017-06-12 22:19:15 -0700

    correctly separate UI and business logic

--- js/scripts.js
+++ js/scripts.js
@@ -1,57 +1,46 @@
 //business logic here:
 var pingPong = function(userInput) {
-  console.log(userInput);
-  var isNumber = function(userInput) {
-    var number = /^[a-zA-Z]+$/;
-    if (userInput.value.match(number)) {
-      return true;
-    } else {
-      return false;
-    }
-  };
+  var number = parseInt(userInput);
+  var integers = [];
 
-  if (isNumber === false || isNaN(userInput) === true) {
+  if ((Number.isInteger(number)) === false || isNaN(userInput) === true) {
     alert("Please enter a number!");
   } else {
-    for (var i = 1; i <= parseInt(userInput); i += 1) {
+    for (var i = 1; i <= number; i += 1) {
       if (parseInt(i) % 3 === 0 && parseInt(i) % 5 === 0) {
-        $("ul").append("<li class='pingpong'>ping-pong</li>");
+        integers.push("ping-pong");
       } else if (parseInt(i) % 5 === 0) {
-        $("ul").append("<li class='pong'>pong</li>");
+        integers.push("pong");
       } else if (parseInt(i) % 3 === 0) {
-        $("ul").append("<li class='ping'>ping</li>");
+        integers.push("ping");
       } else {
-        $("ul").append("<li>" + i + "</li>");
+        integers.push(i);
       }
     };
   }
+  return integers;
 };
 
 var pongPing = function(userInput) {
-  var isNumber = function(userInput) {
-    var number = /^[a-zA-Z]+$/;
-    if (userInput.value.match(number)) {
-      return true;
-    } else {
-      return false;
-    }
-  };
+  var number = parseInt(userInput);
+  var integers = [];
 
-  if (isNumber === false || isNaN(userInput) === true) {
+  if ((Number.isInteger(number)) === false || isNaN(userInput) === true) {
     alert("Please enter a number!");
   } else {
     for (var i = parseInt(userInput); i >= 1; i -= 1) {
       if (parseInt(i) % 3 === 0 && parseInt(i) % 5 === 0) {
-        $("ul").append("<li class='pingpong'>ping-pong</li>");
+        integers.push("ping-pong");
       } else if (parseInt(i) % 5 === 0) {
-        $("ul").append("<li class='pong'>pong</li>");
+        integers.push("pong");
       } else if (parseInt(i) % 3 === 0) {
-        $("ul").append("<li class='ping'>ping</li>");
+        integers.push("ping");
       } else {
-        $("ul").append("<li>" + i + "</li>");
+        integers.push(i);
       }
     };
   }
+  return integers;
 };
 
 //UI logic here:
@@ -62,13 +51,20 @@ $(document).ready(function() {
     $("ul").empty();
     var userInput = $("input#entry").val();
     var result = pingPong(userInput);
+    result.forEach(function(item) {
+      $("ul").append("<li>" + item + "</li>");
+    });
     $(".result").show(result);
   });
   $("#reverse").click(function(event) {
     event.preventDefault();
+
     $("ul").empty();
     var userInput = $("input#entry").val();
     var result = pongPing(userInput);
+    result.forEach(function(item) {
+      $("ul").append("<li>" + item + "</li>");
+    });
     $(".result").show(result);
   });
 });
